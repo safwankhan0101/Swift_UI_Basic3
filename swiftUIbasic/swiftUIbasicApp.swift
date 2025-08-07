2ce61cb4cffbf7bd9b66f150c440be45de0dedac
Safwan khan <safwankhan>
2025-08-07 11:54:57 +0500

    Initial Commit

---
+++ swiftUIbasic/swiftUIbasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIbasicApp.swift
+//  swiftUIbasic
+//
+//  Created by Apple Orchard on 07/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIbasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swiftUIbasicTests/swiftUIbasicTests.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIbasicTests.swift
+//  swiftUIbasicTests
+//
+//  Created by Apple Orchard on 07/08/2025.
+//
+
+import Testing
+@testable import swiftUIbasic
+
+struct swiftUIbasicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
